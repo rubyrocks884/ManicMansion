7ec096526284f40988736b67514874cbcdaa2048
rubyrocks884 <rubyrocks884@gmail.com>
2024-05-28 15:06:10 +0200

    Rensket kode

--- manic_mansion_6.py
+++ manic_mansion_6.py
@@ -9,7 +9,8 @@ pygame.init()
 brettbredde, bretthøyde = 800, 600
 Spillebrett = pygame.display.set_mode([brettbredde, bretthøyde])
 pygame.display.set_caption("Manic Mansion")
-font = pygame.font.SysFont("Arial", 18)
+litenFont = pygame.font.SysFont("Arial", 18)
+storFont = pygame.font.SysFont("Arial", 50)
 
 sidelengde = 30
 menneskefarge = (197, 1, 117)
@@ -18,26 +19,29 @@ spøkelsefarge = (159,25,254)
 hindringfarge = (120,120,120)
 gressfarge = (78,114,0)
 tryggfarge = (193,169,142)
+svart = (0,0,0)
+venstreGrense = brettbredde//4
+høyreGrense = 3*brettbredde//4
 gameover = False
 objekter = [[], [], []]  #sauer, hindringer, spøkelser
 
-def plasserSpilleobjekter(antall):
+def lagSpilleobjekter(antall):
     for n in range(antall):
-      objekter[0].append(Sau(random.randint(3*brettbredde//4, brettbredde-sidelengde), random.randint(0, bretthøyde-sidelengde), sauefarge, "Sau", False))
+      objekter[0].append(Sau(random.randint(høyreGrense, brettbredde-sidelengde), random.randint(0, bretthøyde-sidelengde), sauefarge, "Sau", False))
   
     for n in range(antall):
-      objekter[1].append(Hindring(random.randint(brettbredde//4, 3*brettbredde//4-sidelengde), random.randint(0, bretthøyde-sidelengde), hindringfarge, "Hin"))
+      objekter[1].append(Hindring(random.randint(venstreGrense, høyreGrense-sidelengde), random.randint(0, bretthøyde-sidelengde), hindringfarge, "Hin"))
   
-    objekter[2].append(Spøkelse(random.randint(brettbredde//4, 3*brettbredde//4-sidelengde), random.randint(0, bretthøyde-sidelengde), spøkelsefarge, "Spø", 0.2, 0.2))
+    objekter[2].append(Spøkelse(random.randint(venstreGrense, høyreGrense-sidelengde), random.randint(0, bretthøyde-sidelengde), spøkelsefarge, "Spø", 0.2, 0.2))
   
 
 def main():
   global gameover
   global objekter
-  global font
+  global storFont
 
   menneske = Menneske(brettbredde//8-sidelengde/2, bretthøyde/2, menneskefarge, "Deg",  0.5, 0, False)
-  plasserSpilleobjekter(3)
+  lagSpilleobjekter(3)
   
   run = True
   while run:
@@ -45,52 +49,48 @@ def main():
       if event.type == pygame.QUIT:
         run=False
     
-    frisone_venstre = pygame.Rect(0, 0, brettbredde//4, bretthøyde)
-    midten = pygame.Rect(brettbredde//4, 0, brettbredde/2, bretthøyde)
-    frisone_høyre = pygame.Rect(3*brettbredde//4, 0, brettbredde//4, bretthøyde)
+    frisone_venstre = pygame.Rect(0, 0, venstreGrense, bretthøyde)
+    midten = pygame.Rect(venstreGrense, 0, brettbredde/2, bretthøyde)
+    frisone_høyre = pygame.Rect(høyreGrense, 0, brettbredde//4, bretthøyde)
     Spillebrett.fill(tryggfarge, frisone_venstre)
-    Spillebrett.fill((0,0,0), midten)
+    Spillebrett.fill(svart, midten)
     Spillebrett.fill(gressfarge, frisone_høyre)
-    poengScore = font.render(f"Poeng: {menneske.poeng}",True, (0,0,0), tryggfarge)
-    Spillebrett.blit(poengScore, (5,2))
+    
+    menneske.visPoeng(Spillebrett)
     
     tastnå = pygame.key.get_pressed() # Henter en liste med status for alle tastatur-tasteliste
     menneske.beveg(tastnå)
-    menneske.plassering()
+    menneske.tegn()
               
     #for å hindre at de lander oppå hverandre        
     for sau1 in objekter[0]:
       for sau2 in objekter[0]:
         if sau1 != sau2:
           if sau1.sjekkKollisjon(sau2):
-              sau1.xPosisjon = random.randint(3*brettbredde//4, brettbredde-sidelengde)
+              sau1.xPosisjon = random.randint(høyreGrense, brettbredde-sidelengde)
               sau1.yPosisjon = random.randint(0, bretthøyde-sidelengde)   
     
-    for hind1 in objekter[0]:
-      for hind2 in objekter[0]:
+    for hind1 in objekter[1]:
+      for hind2 in objekter[1]:
         if hind1 != hind2:
           if hind1.sjekkKollisjon(hind2):
-              hind1.xPosisjon = random.randint(3*brettbredde//4, brettbredde-sidelengde)
+              hind1.xPosisjon = random.randint(venstreGrense, høyreGrense-sidelengde)
               hind1.yPosisjon = random.randint(0, bretthøyde-sidelengde)         
-                
+            
+    for liste in objekter:
+      for spillobjekt in liste:
+        spillobjekt.tegn()            
     
     for spøk in objekter[2]: 
       spøk.flyt()
-      spøk.endreRetning()
-      
       if menneske.sjekkKollisjon(spøk): #treffer spøkelse
         gameover = True
     
-    for liste in objekter:
-      for spillobjekt in liste:
-        spillobjekt.plassering()
-    
     if gameover == True:
-      font = pygame.font.SysFont("Arial", 50)
-      gameovertekst = font.render("GAME OVER", True, (255, 255, 255), (255,0,0))
+      gameovertekst = storFont.render("GAME OVER", True, (255, 255, 255), (255,0,0))
       text_rect = gameovertekst.get_rect(center=(brettbredde/2,bretthøyde/2))
       Spillebrett.blit(gameovertekst, text_rect)
-      poengtekst = font.render(f"Score: {menneske.poeng}", True, (0,0,0), (242,169,255))
+      poengtekst = storFont.render(f"Score: {menneske.poeng}", True, (0,0,0), (242,169,255))
       poengrekt = poengtekst.get_rect(center = (brettbredde/2, 2.3*bretthøyde/3))
       Spillebrett.blit(poengtekst, poengrekt)
       
@@ -100,21 +100,23 @@ def main():
         menneske.bærSau()
         sau.fjernSau()
 
-    menneske.økPoeng()
+    menneske.evtØkPoeng()
 
     pygame.display.update()
   pygame.quit()
 
 class SpillObjekt:
+  global litenFont
+  
   def __init__(self, xPosisjon:int, yPosisjon:int, farge, navn):
     self.xPosisjon = xPosisjon
     self.yPosisjon = yPosisjon
     self.farge = farge
     self.navn = navn
   
-  def plassering(self):
+  def tegn(self):
     pygame.draw.rect(Spillebrett, self.farge, (self.xPosisjon, self.yPosisjon, sidelengde, sidelengde))
-    firkant = font.render(self.navn, True, (0,0,0), self.farge)
+    firkant = litenFont.render(self.navn, True, (0,0,0), self.farge)
     Spillebrett.blit(firkant, (self.xPosisjon+2, self.yPosisjon+2))
     
   def sjekkKollisjon(self, objekt): #trykk Fn + F2 for å bytte ut variablene i funksjonen
@@ -128,9 +130,6 @@ class SpillObjekt:
       return False
     else:
       return True
-  
-  def flytt(self):
-    pass
 
 class Menneske(SpillObjekt):
   def __init__(self, xPosisjon:int, yPosisjon:int, farge, navn, fart:int, poeng:int, bærerSau:bool):
@@ -167,25 +166,25 @@ class Menneske(SpillObjekt):
           self.xPosisjon = gammelx
           self.yPosisjon = gammely
   
-  def reduserFart(self):
-    self.fart -= 0.05
-  
-  def økPoeng(self):
+  def evtØkPoeng(self):
     if 0 < self.xPosisjon < brettbredde/4 and self.bærerSau == True:
       self.bærerSau = False
       self.navn = "Deg"
       self.poeng += 1
-      print(self.poeng)
-      self.fart += 0.05
-      plasserSpilleobjekter(1)
+      self.fart += 0.2
+      lagSpilleobjekter(1)
   
   def bærSau(self):
     global gameover
     if self.bærerSau:
       self.navn = "D+1"
-      self.fart -= 0.05
+      self.fart -= 0.2
       if len(objekter[0]) == 2:
         gameover = True
+        
+  def visPoeng(self, vindunavn):
+    poengScore = litenFont.render(f"Poeng: {self.poeng}",True, svart, tryggfarge)
+    vindunavn.blit(poengScore, (5,2))
         
 class Spøkelse(SpillObjekt):
   def __init__(self, xPosisjon:int, yPosisjon:int, farge, navn, xfart, yfart):
@@ -198,31 +197,22 @@ class Spøkelse(SpillObjekt):
       self.xPosisjon += self.xfart
       self.yPosisjon += self.yfart
     
-  def endreRetning(self):
     if (self.xPosisjon <= brettbredde // 4 or self.xPosisjon >= 3 * brettbredde // 4 - sidelengde):
       self.xfart = -self.xfart
       self.xPosisjon += self.xfart
     if (self.yPosisjon <= 0 or self.yPosisjon >= bretthøyde - sidelengde):
       self.yfart = -self.yfart
       self.yPosisjon += self.yfart
-
   
 class Hindring(SpillObjekt):
   def __init__(self, xPosisjon:int, yPosisjon:int, farge, navn):
     super().__init__(xPosisjon, yPosisjon, farge, navn)
   
 class Sau(SpillObjekt):
-  def __init__ (self, xPosisjon:int, yPosisjon:int, farge, navn, blirBåret:bool):
+  def __init__ (self, xPosisjon:int, yPosisjon:int, farge, navn):
     super().__init__(xPosisjon, yPosisjon, farge, navn)    
-    self.blirBåret = blirBåret
-    
-  def blirLøftet(self, menneske):
-    self.xPosisjon = menneske.xPosisjon + sidelengde
-    self.yPosisjon = menneske.yPosisjon
   
   def fjernSau(self):
     objekter[0].remove(self)
     
 main()
-
-        
